a2e024ac0051d93e5182bbf51fb2be399db93e9e
Fernando Meca <fernando_mecca@hotmail.com>
2022-03-11 23:56:55 -0300

    Adicionado arquivo .gitignore e atulizado arquivo TesteEmpregado.java

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -19,12 +19,5 @@ public class TesteEmpregado {
         System.out.println (e1.calculaSalario());
         System.out.println (e2.calculaSalario());
         System.out.println (e3.calculaSalario());
-
-
-
-
-
-
-
     }
 }
